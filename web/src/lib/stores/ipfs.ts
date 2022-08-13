62eb425ce03cfb9bf8ebffffc50765c1ec8ec95c
tjvsx <vanslotj@gmail.com>
2022-08-13 17:54:40 -0400

    temporary fix

--- web/src/lib/stores/ipfs.ts
+++ web/src/lib/stores/ipfs.ts
@@ -1,5 +1,20 @@
-export const ssr = false;
+export const ssr = true;
+import { create } from 'ipfs-http-client';
 import { writable } from 'svelte/store';
-// import type ethers from '@typechain/ethers-v5';
 
 export const ipfs = writable<any>();
+
+export const start = async () => {
+
+  const auth =
+  'Basic MkNDMHd0Tmg5NTFZQ2l1ZUdZZURRRkRLMjhSOmUwNTFkOWY1NzQ3ODhlMTg4NGQ1N2EyZGVjZjVmZTNi';
+
+  ipfs.set(create({
+    host: 'ipfs.infura.io',
+    port: 5001,
+    protocol: 'https',
+    headers: {
+        authorization: auth,
+    },
+  }));
+}
--- web/src/routes/index.svelte
+++ web/src/routes/index.svelte
@@ -1,53 +1,19 @@
 <script lang="ts">
-	import { onDestroy, onMount } from 'svelte';
-	import { ethers } from 'ethers';
-  import { ipfs } from '$lib/stores/ipfs';
+	import { onMount } from 'svelte';
+  import { connected, accountChainId } from '$lib/stores/provider';
+  import { start } from '../lib/stores/ipfs'
   import { populate } from '$lib/stores/contract';
 	import Menu from "$lib/components/Menu.svelte";
 	import Main from "$lib/components/Main.svelte";
   import Profile from '$lib/components/Profile.svelte';
   import Background from '$lib/components/Background.svelte';
-  import { connected, accountChainId } from '$lib/stores/provider';
   import Wallet from '$lib/components/Wallet.svelte';
-  import { create } from 'ipfs-client';
   import Notifications from 'svelte-notifications';
 
 	onMount(async () => {
-
-    if (!$ipfs) {
-      $ipfs = create({
-        http: 'https://ipfs.infura.io:5001/api/v0'
-      })
-      console.log('ipfs using client...')
-      // const IPFSmodule = await import('../modules/ipfs-core/ipfs-core.js');
-      // const IPFS = IPFSmodule.default;
-      // $ipfs = await IPFS.create();  /// TODO: set timeout?? -- does this need a local ipfs node running
-      // console.log('ipfs client overridden...')
-      // console.log(globalThis)
-    } else {
-      console.info('already initiated', $ipfs)
-    }
-
+    await start();
     await populate();
-
-    // const provider = new ethers.providers.Web3Provider(globalThis.ethereum);
-    // provider.on('block', async (blockNumber) => {
-    //   console.log('block', blockNumber)
-    // });
-    // provider.on("error", (tx) => {
-    //   console.log('tx error', tx)
-    // });
-
 	});
-
-  // onDestroy(async() => {
-  //   if ($ipfs) {
-  //     console.info('the ipfs node is being stopped');
-  //     //@ts-ignore
-  //     $ipfs.stop();
-  //     globalThis.ipfsNode = null;
-  //   }
-  // })
 
   let width: number;
 	let left: number;
